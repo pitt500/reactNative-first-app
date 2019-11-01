d7e2c3561a62459537196116312a679b932e92cf
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-01 11:24:10 -0700

    Add action to delete goal item

--- App.js
+++ App.js
@@ -4,21 +4,30 @@ import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
-  
+
   const [courseGoals, setCourseGoals] = useState([]);
 
   const addGoalHandler = goalTitle => {
-    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle }]);
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
   };
 
   return (
     <View>
       <View style={styles.screen}>
-        <GoalInput onAddGoal={addGoalHandler}/>
+        <GoalInput onAddGoal={addGoalHandler} />
         <FlatList
           keyExtractor={(item, index) => item.id}
           data={courseGoals}
-          renderItem={itemData => <GoalItem title={itemData.item.value} />}
+          renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />}
         />
       </View>
     </View>
