3b7ef0aae588390056c0dc0934f28ea05a7dd206
pitt500 <pedro.rojaslo50@gmail.com>
2019-10-31 19:09:04 -0700

    Add support for FlatList instead of ScrollView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -10,11 +10,11 @@ export default function App() {
   };
 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: enteredGoal }]);
   };
 
   return (
-    <ScrollView>
+    <View>
       <View style={styles.screen}>
         <View style={styles.inputContainer}>
           <TextInput
@@ -25,15 +25,19 @@ export default function App() {
           />
           <Button title="Add" onPress={addGoalHandler} />
         </View>
-        <View>
-          {courseGoals.map((goal) => (
-            <View key={goal} style={styles.listItem}>
-              <Text>{goal}</Text>
+        <FlatList 
+          keyExtractor={(item, index) => item.id}
+          data={courseGoals} 
+          renderItem={itemData => (
+            <View style={styles.listItem}>
+              <Text>{itemData.item.value}</Text>
             </View>
-          ))}
-        </View>
+          )}
+        />
+
+
       </View>
-    </ScrollView>
+    </View>
   );
 }
 
